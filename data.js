36cfae26f7843393b36d667a26ba42a8c24bb3ce
Quoc Huy Lieu <91789128+Huy-Lieu@users.noreply.github.com>
2026-08-03 12:24:20 -0700

    note 2026-08-03 via studio

--- data.js
+++ data.js
@@ -145,6 +145,12 @@ const SITE_DATA = {
     {
       date: "2026-08-03",
       mood: "💡",
+      tags: ["website"],
+      text: "Consider to add the abilities to do custom tag when import the .md in the studio\n—> current only pre-defined tag."
+    },
+    {
+      date: "2026-08-03",
+      mood: "💡",
       tags: ["learning", "career", "C"],
       text: "I have learnt the SDLC. Union and Struct. I feel confident again to look for job. \n\nThis time customize my resume better. Now I kinda knew what things I have to do. Need to speak the same language as the JD. (At least get me the phone call)"
     },
